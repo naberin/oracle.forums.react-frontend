cf2939d4fe1e9408db06c15750529ecac8880f54
naberin <aberinnj@gmail.com>
2022-03-30 10:13:29 -0500

    Cleaned app and updated design and structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <div id="app" className="flex flex-col">
+        <AppHeader className={"flex-grow-1"}/>
+    </div>
+  );
+}
+
+export default App;
